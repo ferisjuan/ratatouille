deef53549050c3e4453c85b50dd75758678c5d6f
ferisjuan <feris.juan@gmail.com>
2019-07-30 17:37:52 -0500

    Updated header navigation

--- dist/out-tsc/src/app/header/header.component.js
+++ dist/out-tsc/src/app/header/header.component.js
@@ -1,16 +1,7 @@
 import * as tslib_1 from "tslib";
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 let HeaderComponent = class HeaderComponent {
-    constructor() {
-        this.featureSelected = new EventEmitter();
-    }
-    onSelect(feature) {
-        this.featureSelected.emit(feature);
-    }
 };
-tslib_1.__decorate([
-    Output()
-], HeaderComponent.prototype, "featureSelected", void 0);
 HeaderComponent = tslib_1.__decorate([
     Component({
         selector: 'app-header',
