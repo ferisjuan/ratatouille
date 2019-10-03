08114877ba86103a1addc4b00f45a23ac477eec1
ferisjuan <feris.juan@gmail.com>
2019-10-03 10:32:45 -0500

    Updates

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,16 @@
 import { Ingredient } from '../shared/ingredient.model';
 
 import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
   startedEditing = new Subject<number>();
-  private ingredients: Ingredient[] = [new Ingredient('Salt', 3), new Ingredient('Meat', 2)];
+  private ingredients: Ingredient[] = [
+    new Ingredient('Salt', 3),
+    new Ingredient('Meat', 2),
+  ];
 
   getIngredients() {
     return this.ingredients.slice();
